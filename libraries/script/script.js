6d10a863fc834e48c66da6b5d812c0cfa39b57eb
ilhamnopihendri <ilhamnopihendri@gmail.com>
2021-04-01 15:24:54 +0700

    menyelesaikan sampai form login

---
+++ libraries/script/script.js
@@ -0,0 +1,57 @@
+// Smooth scroll
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
+
+//Get the button
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+// when user click on the button, scroll to the top of the document
+function topFunction() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+}
+
+// show hide password
+const password = document.getElementById("password");
+const button_show_hide = document.getElementById("button_show_hide");
+
+function showHide() {
+  if (password.type === "password") {
+    password.setAttribute("type", "text");
+    button_show_hide.classList.add("hide");
+  } else {
+    password.setAttribute("type", "password");
+    button_show_hide.classList.remove("hide");
+  }
+}
